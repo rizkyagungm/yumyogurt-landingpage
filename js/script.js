8f7bbc2c4f3d5c95d27762dabb8c75bad28bde0f
Rizky Agung Mahendra <rizkyagungmahendra88@gmail.com>
2025-08-21 22:43:38 +0700

    finish landingpage YumYogurt project

---
+++ js/script.js
@@ -0,0 +1,9 @@
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".main-header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
